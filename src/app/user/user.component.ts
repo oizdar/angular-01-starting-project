652d77df53d4ca811433c6a7279cdaa4eed8ff09
Radosław Malinowski <rmalinowski@booksfactory.pl>
2025-07-30 15:07:42 +0200

    WIP 48 - ready: task structure, users, custom event, inputs, models

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
+import { User } from "./user.model";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>()
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`;
+  }
+  // imagePath = computed<string>(() => 'assets/users/' + this.avatar());
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
